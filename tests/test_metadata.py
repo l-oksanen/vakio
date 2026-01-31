4001983b4d5388f81dc100268aa26ed0be6613e0
Lauri Oksanen <lauri.oksanen@helsinki.fi>
2026-01-31 15:04:36 +0200

    Consistency tests for greys

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -1,4 +1,32 @@
 from vakio import metadata
+from vakio.palette import create_palette
+from vakio.view import hex_to_oklch
+
+
+def assert_grey_order(greys):
+    def lightness(h):
+        return hex_to_oklch(h)[0]
+
+    assert [str(h) for h in greys] == sorted(greys, key=lightness)
+
+
+def test_grey_order():
+    palette = create_palette()
+    assert_grey_order(palette[metadata.GREYS])
+    assert_grey_order(palette[metadata.UI_LIGHT_GREYS])
+    assert_grey_order(palette[metadata.MANUAL_GREYS])
+    assert_grey_order(palette[metadata.UI_GREYS])
+    assert_grey_order(palette[metadata.TEXT_GREYS])
+
+
+def test_grey_text_ui_disjoint():
+    assert set(metadata.UI_GREYS).isdisjoint(set(metadata.TEXT_GREYS))
+
+
+def test_grey_text_ui_union():
+    assert set(metadata.UI_GREYS) | set(metadata.TEXT_GREYS) == set(
+        metadata.GREYS
+    )
 
 
 def test_semantic_mapping_injective():
